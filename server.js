f127097247f6e5be3ea6f24021325523d584696a
Josh Pitman <shidogrey@gmail.com>
2016-03-28 13:01:11 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    logger  = require('morgan')('dev'),
+    server  = express();
+
+
+server.use(logger);
+server.use(express.static(__dirname+'/public'));
+server.set('port',(process.env.PORT || 8080));
+
+server.get('/', home);
+
+
+server.listen(server.get('port'), listenCallback);
+
+
+function home(req, res){
+  res.sendFile('public/html/index.html', {root: __dirname});
+}
+
+
+function listenCallback(){
+  console.log('nowlistening on port' + server.get('port'));
+}
